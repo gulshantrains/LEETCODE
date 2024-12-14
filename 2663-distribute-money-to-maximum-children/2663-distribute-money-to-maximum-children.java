e90a057baa0a435471271ad0aceefbdebddf2035
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2024-12-14 13:45:42 +0530

    Time: 2 ms (56.85%), Space: 41.9 MB (53.2%) - LeetHub

---
+++ 2663-distribute-money-to-maximum-children/2663-distribute-money-to-maximum-children.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int distMoney(int money, int children) {
+
+        if ((float)money / (float)children == (float)8)
+            return children;
+        if (money < children)
+            return -1;
+
+        int cnt = 0;
+        money -= children;
+
+        while (money >= 7 && children > 0) {
+            money -= 7;
+            children--;
+            cnt++;
+        }
+        if (cnt == 0)
+            return cnt;
+        else {
+            if (money > 0 && children == 0)
+                cnt -= 1;
+            if (money == 3 && children == 1)
+                cnt -= 1;
+        }
+        return cnt;
+
+    }
+}
