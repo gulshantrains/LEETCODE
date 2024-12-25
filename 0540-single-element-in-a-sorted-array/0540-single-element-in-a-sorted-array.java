af5cf68abd29869dcdd06dea425136c398d6b7b0
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2024-12-25 18:39:11 +0530

    Time: 0 ms (100%), Space: 50.6 MB (14.58%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,13 +1,40 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        if(nums.length==1) return nums[0];
+        return nums[bs(nums)];
+    }
 
-        for (var x : nums) {
-            map.put(x, map.getOrDefault(x, 0) + 1);
-        }
-        for (Map.Entry<Integer, Integer> xx : map.entrySet()) {
-            if (xx.getValue() == 1)
-                return xx.getKey();
+    private int bs(int[] a) {
+        int l = 0, r = a.length - 1;
+        int n = a.length;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (mid < n-1 && mid > 0) {
+                if (a[mid] != a[mid + 1] && a[mid] != a[mid - 1])
+                    return mid;
+            }
+            if (mid == 0 && a[mid] != a[mid + 1]) {
+                return mid;
+            }
+            if (mid == n-1 && a[mid] != a[mid - 1]) {
+                return mid;
+            }
+
+            if (mid < n - 1 && a[mid] == a[mid + 1]) {
+                if (mid % 2 != 0) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 2;
+                }
+            } else {
+                if (mid % 2 == 0) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
         }
         return -1;
     }
