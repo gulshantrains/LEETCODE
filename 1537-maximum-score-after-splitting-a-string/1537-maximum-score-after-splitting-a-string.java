e9a7237860ea34feacac8cb601ed37edb639ba92
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-01 11:16:39 +0530

    Time: 1 ms (97.36%), Space: 41.5 MB (64.03%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,31 +1,22 @@
 class Solution {
     public int maxScore(String s) {
         int n = s.length();
+        int oneCount = 0, zeroCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1')
+                oneCount++;
+        }
         int ans = 0;
-        StringBuilder left = new StringBuilder();
-
         for (int i = 0; i < n - 1; i++) {
-            int cnt = 0;
-            StringBuilder right = new StringBuilder();
-            left.append(s.substring(i, i + 1));
-            right.append(s.substring(i + 1, n));
-
-            System.out.println(left);
-            System.out.println(right);
-
-            for (int j = 0; j < left.length(); j++) {
-                if (left.charAt(j) == '0')
-                    cnt++;
+            if (s.charAt(i) == '1') {
+                oneCount--;
+            } else {
+                zeroCount++;
             }
-            System.out.println(cnt);
-            for (int k = 0; k < right.length(); k++) {
-                if (right.charAt(k) == '1')
-                    cnt++;
-            }
-            System.out.println(cnt);
-            ans = Math.max(ans, cnt);
-
+            ans = Math.max(ans, (oneCount + zeroCount));
         }
         return ans;
+
     }
 }
