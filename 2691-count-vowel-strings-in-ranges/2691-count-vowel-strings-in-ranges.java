e06f2a54660c6978784471b7353250801bd1c938
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-02 16:51:03 +0530

    Time: 104 ms (5.01%), Space: 90.8 MB (9.11%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,16 +1,25 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
         int[] ans = new int[queries.length];
-        int[] count = new int[words.length + 1];
+        int[] count = new int[words.length];
         HashSet<String> set = new HashSet<>();
 
         for (int i = 0; i < words.length; i++) {
             String x = words[i];
             if (x.matches("^[aeiou]$") || x.matches("^[aeiou].*[aeiou]$")) {
-                count[i + 1] = count[i] + 1;
-                set.add(x);
+                if (i == 0) {
+                    count[i] = 1;
+                    set.add(x);
+                } else {
+                    count[i] = count[i - 1] + 1;
+                    set.add(x);
+                }
+
             } else {
-                count[i + 1] = count[i];
+                if (i == 0) {
+                    count[i] = 0;
+                } else
+                    count[i] = count[i - 1];
             }
 
         }
@@ -19,7 +28,12 @@ class Solution {
             int start = queries[i][0];
             int end = queries[i][1];
 
-            ans[i] = count[end + 1] - count[start];
+            if(start==0){
+                ans[i]=count[end];
+            }else{
+                 ans[i] = count[end]-count[start-1];
+            }
+           
         }
         return ans;
     }
