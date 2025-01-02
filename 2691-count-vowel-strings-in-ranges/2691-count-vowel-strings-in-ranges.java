33ea46b59d117495cb6887003aa90ab8db93783a
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-02 17:05:37 +0530

    Time: 4 ms (98.18%), Space: 85.7 MB (76.77%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -4,12 +4,12 @@ class Solution {
         int[] ans = new int[queries.length];
 
         for (int i = 0; i < words.length; i++) {
-            if (words[i].matches("^[aeiou]$") || words[i].matches("^[aeiou].*[aeiou]$"))
+            if (isVowelString(words[i]))
                 prefixSum[i + 1] = prefixSum[i] + 1;
-
             else
                 prefixSum[i + 1] = prefixSum[i];
         }
+        
         for (int j = 0; j < queries.length; j++) {
             int start = queries[j][0];
             int end = queries[j][1];
@@ -17,5 +17,13 @@ class Solution {
             ans[j] = prefixSum[end + 1] - prefixSum[start];
         }
         return ans;
+    }
+
+    public boolean isVowelString(String str) {
+        return isVowel(str.charAt(0)) && isVowel(str.charAt(str.length() - 1));
+    }
+
+    public boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 }
