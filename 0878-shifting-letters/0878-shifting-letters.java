5732b1d7437d5fecba5336b575af4ae9e9ba5b7e
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-05 18:09:43 +0530

    Time: 9 ms (57.14%), Space: 57.2 MB (60.13%) - LeetHub

---
+++ 0878-shifting-letters/0878-shifting-letters.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String shiftingLetters(String s, int[] shifts) {
+        int n = s.length();
+
+        for (int i = n - 2; i >= 0; i--) {
+            shifts[i] = (shifts[i] + shifts[i + 1]) % 26;
+        }
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char x = s.charAt(i);
+            // Total shifts (0-25)
+            int sf = shifts[i];
+
+            char xx = (char) ((x - 'a' + sf) % 26 + 'a');
+            ans.append(xx);
+        }
+        return ans.toString();
+    }
+}
