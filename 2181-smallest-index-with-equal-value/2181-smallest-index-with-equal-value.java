2e650d6360bc38a04ee8eab052a569c925ec4273
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-07 23:59:04 +0530

    Time: 1 ms (10.45%), Space: 44.9 MB (9.65%) - LeetHub

--- 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.java
+++ 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.java
@@ -1,14 +1,12 @@
 class Solution {
     public int smallestEqual(int[] nums) {
-        int ans = -1;
+        int ans = Integer.MAX_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
-            if (i % 10 == nums[i]) {
-                if (ans == -1)
-                    ans = i;
-                ans = Math.min(ans, i);
+            if (i % 10 == nums[i] && ans > i) {
+                ans = i;
             }
         }
-        return ans;
+        return (ans == Integer.MAX_VALUE ? -1 : ans);
     }
 }
