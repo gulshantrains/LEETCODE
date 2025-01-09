6dbddf125a6e4121549ed569de51ae2b58b24515
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-10 00:48:05 +0530

    Time: 118 ms (21.45%), Space: 108.7 MB (12.21%) - LeetHub

--- 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.java
+++ 1977-minimum-interval-to-include-each-query/1977-minimum-interval-to-include-each-query.java
@@ -16,18 +16,18 @@ class Solution {
         HashMap<Integer, Integer> mp = new HashMap<>();
 
         for (int x : res) {
-            while (i < m && A[i][0] <= x) {
-                int r = A[i][1], l = A[i++][0];
+            while (i < m && A[i][0] <= x) {      //if queries is <= to left value then add means there
+                int r = A[i][1], l = A[i++][0];  //may be chance that [] lies in range
                 mt.put(r - l + 1, r);
             }
-            while (!mt.isEmpty() && mt.firstEntry().getValue() < x) {
-                mt.pollFirstEntry();
+            while (!mt.isEmpty() && mt.firstEntry().getValue() < x) { //if value of query is greater
+                mt.pollFirstEntry();                //means that query doesnt lies in..[] so remove
             }
-            mp.put(x, (mt.isEmpty() ? -1 : mt.firstKey()));
+            mp.put(x, (mt.isEmpty() ? -1 : mt.firstKey())); //if something available then put in map
         }
         i = 0;
         for (int xx : queries) {
-            ans[i++] = mp.get(xx);
+            ans[i++] = mp.get(xx); //re-write with original index
         }
 
         return ans;
