eb47b69445b8a450ac9e63afa57d883011d9287a
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-13 11:45:32 +0530

    Time: 9 ms (92.05%), Space: 46.7 MB (25.83%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,24 +1,22 @@
 class Solution {
     public int minimumLength(String s) {
         if (s.length() == 2)
-            return 2;
+            return 2; // If s length is 2 then directly return its length
         int[] ar = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            char xx = s.charAt(i);
-            ar[xx - 'a']++;
+            ar[s.charAt(i) - 'a']++; // Count evry digit occurance
         }
 
         int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            int val = ar[i];
+        for (var val : ar) {
 
-            if (val > 3 && val % 2 == 0)
-                ans += 2;
-            else if (val == 3 || (val > 3 && val % 2 != 0))
-                ans += 1;
+            if (val > 3 && val % 2 == 0) // agr even times(2,4,22) occur krta hai
+                ans += 2; // tb woo 2 baar he bache g
+            else if (val == 3 || (val > 3 && val % 2 != 0)) // if odd times
+                ans += 1; // or ==3 then sirf ek baar bache g woo
             else
-                ans += val;
+                ans += val; // all other case me uske value ko count kro
         }
         return ans;
     }
