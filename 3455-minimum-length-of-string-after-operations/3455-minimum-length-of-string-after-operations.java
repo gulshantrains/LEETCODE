6e6bb30286f3992019704ad3849282155eb550f4
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-13 11:40:54 +0530

    Time: 9 ms (92.05%), Space: 46.3 MB (53.64%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,5 +1,7 @@
 class Solution {
     public int minimumLength(String s) {
+        if (s.length() == 2)
+            return 2;
         int[] ar = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
@@ -10,11 +12,10 @@ class Solution {
         int ans = 0;
         for (int i = 0; i < 26; i++) {
             int val = ar[i];
-            if (val == 3)
-                ans += 1;
-            else if (val > 3 && val % 2 == 0)
+
+            if (val > 3 && val % 2 == 0)
                 ans += 2;
-            else if (val > 3 && val % 2 != 0)
+            else if (val == 3 || (val > 3 && val % 2 != 0))
                 ans += 1;
             else
                 ans += val;
