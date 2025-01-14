e75eacaea24bbfc87522d8a2c1f55815878d6a06
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-14 18:08:41 +0530

    Time: 2 ms (95.83%), Space: 45.7 MB (28.65%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        int[] freq = new int[50]; //Freq array lete hai to count number appread till i index
+        int[] freq = new int[A.length + 1]; // Freq array lete hai to count number appread till i index
         int[] ans = new int[A.length];
-        //TC->O(n)
+        // TC->O(n)
+        int cnt = 0;
         for (int i = 0; i < A.length; i++) {
-            freq[A[i] - 1]++; // Increase the count of number jo A me aaya hai
-            freq[B[i] - 1]++; // Increase the count of number jo B me aaya hai
+            freq[A[i]]++; // Increase the count of number jo A me aaya hai
+            // Increase the count of number jo B me aaya hai
 
-            int cnt = 0;
-            //TC->O(51) or O(n) ===>O(n*51)
-            for (int x : freq) {
-                if (x == 2) //Agar count 2 ho gaya hai means ki woo common hai 
-                    cnt++;
-            }
+            if (freq[A[i]] == 2)
+                cnt++;
+
+            freq[B[i]]++;
+            if (freq[B[i]] == 2)
+                cnt++;
+
             ans[i] = cnt;
         }
         return ans;
