372308b897af97a214f16f1622fbd48abd1ee116
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-15 19:39:40 +0530

    Time: 0 ms (100%), Space: 40.8 MB (39.62%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.java
+++ 2509-minimize-xor/2509-minimize-xor.java
@@ -1,40 +1,41 @@
 class Solution {
     public boolean isSet(int x, int bit) {
-        return (x & (1 << bit)) != 0;
+        return (x & (1 << bit)) != 0; //agr LSB p 0 NHI hai to SET hai 
     }
 
     public int Set(int x, int bit) {
-        return x | (1 << bit);
+        return x | (1 << bit); //Left shift till required bit and then ans with x
     }
 
     public int unSet(int x, int bit) {
-        return x & ~(1 << bit);
+        return x & ~(1 << bit); //left shift till required and then take AND with negation
     }
 
     public int minimizeXor(int num1, int num2) {
-        int x = num1;
-        int reqdbitCount = Integer.bitCount(num2);
-        int orgbitCount = Integer.bitCount(x);
+        int x = num1;  //phle se he ans=num1 le lete hai soo agr 0 aa gaya to maze maze
 
-        int bit = 0;
+        int reqdbitCount = Integer.bitCount(num2); //Required Set Bit
+        int orgbitCount = Integer.bitCount(x); //Hmre number me kitna set bit hai
 
-        if (reqdbitCount > orgbitCount) {
-            while (reqdbitCount > orgbitCount) {
-                if (!isSet(x, bit)) {
-                    x = Set(x, bit);
-                    orgbitCount++;
+        int bit = 0; //bit is at 0 means LSB
+
+        if (reqdbitCount > orgbitCount) { //agr required set bit BIG hai original se then we must Set some bit (ADD 1 in number)
+            while (reqdbitCount > orgbitCount) { //jb tak equal n ho jye
+                if (!isSet(x, bit)) { //kya LSB 0 hai agr hai to usse 1 me convert kre g || agr 1 hai already to move
+                    x = Set(x, bit);  //set kro bhai jldi
+                    orgbitCount++; //set bit count increase kre g 
                 }
-                bit++;
+                bit++; //1 hai LSB p soo move to next bit to check
             }
-        } else if (reqdbitCount < orgbitCount) {
+        } else if (reqdbitCount < orgbitCount) { //agr small hai to unset kre g means REMOVE 1
             while (reqdbitCount < orgbitCount) {
-                if (isSet(x, bit)) {
+                if (isSet(x, bit)) { //Kya LSB p 1 hai..agr hai to unset kre g wrna next search kro 
                     x = unSet(x, bit);
                     orgbitCount--;
                 }
-                bit++;
+                bit++; //already 0  hai so move to next bit to find 1
             }
         }
-        return x;
+        return x; 
     }
 }
