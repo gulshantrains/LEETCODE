094070117253b78ffc2e1526c9894516a5c7c161
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-16 16:40:04 +0530

    Time: 1 ms (100%), Space: 61.1 MB (74.14%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.java
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.java
@@ -1,17 +1,20 @@
 class Solution {
     public int xorAllNums(int[] A, int[] B) {
         int xorA = 0, xorB = 0, result = 0;
+    //XOR is associaive in nature (a ^ b=b^a)
+    //XOR is commututative in nature (a^b))^c=a^(b^c)
+
 
         for (int x : A)
-            xorA ^= x;
+            xorA ^= x; //Take xor of A
         for (int x : B)
-            xorB ^= x;
+            xorB ^= x; //take xor of B
 
         if (A.length % 2 == 1)
-            result ^= xorB;
+            result ^= xorB; //If a length is odd then B will contribute otherwsise not
 
         if (B.length % 2 == 1)
-            result ^= xorA;
+            result ^= xorA; //if B length is odd then it will conntribute 
 
         return result;
     }
