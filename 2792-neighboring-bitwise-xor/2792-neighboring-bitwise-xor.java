c9b9a390e53a14cc21ec648ae6e13e64e98b8fb4
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-17 23:50:58 +0530

    Time: 11 ms (10%), Space: 63.5 MB (6.47%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean doesValidArrayExist(int[] d) {
-        int res = 0;
-        for (var x : d) {
-            res ^= x;
-        }
-        return res == 0;
+        return Arrays.stream(d).sum() % 2 == 0;
     }
 }
