186abc12cd8bae39c22fa18adb0715527c49ec68
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-18 19:48:34 +0530

    Time: 47 ms (76.3%), Space: 48.1 MB (17.07%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -5,6 +5,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
 
+         //Jaise he size 2 se jda hua window srink kre g left side se
             while (map.size() > k) {
                 map.put(nums[j], map.get(nums[j]) - 1);
 
@@ -13,12 +14,16 @@ class Solution {
                 }
                 j++;
             }
-            cnt += (i - j + 1);
+            cnt += (i - j + 1); //No of subArray ending at J ka formula hai
         }
         return cnt;
     }
 
     public int subarraysWithKDistinct(int[] nums, int k) {
+        //Agr we know till <=k & <=k-1 then Difference dono ka will
+        //give exactly K
         return subArray(nums, k) - subArray(nums, k - 1);
     }
 }
+//Dikkat kya ho rha tha ki hm log [1,2,2] ko ek count kr rahe the
+//jbki ussme [1,2,2] & [2,2]--> 2 hai
