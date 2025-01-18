ab95b291db7b37086daacf080745a4ce55dcfca0
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-18 17:20:01 +0530

    Time: 96 ms (37.26%), Space: 61 MB (5.02%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -17,12 +17,15 @@ class Solution {
         int j = 0, ans = 0;
 
         for (int i = 0; i < n; i++) {
-
-            max.add(new Pair(nums[i], i));
+        //Add both to min and max heap to get in O(1)
+            max.add(new Pair(nums[i], i)); 
             min.add(new Pair(nums[i], i));
 
-            if (!max.isEmpty() && !min.isEmpty() &&
+        //If it is greater than limit then move one pointer jidhar se dikkat n ho
+
+            while (!max.isEmpty() && !min.isEmpty() &&
                     max.peek().val - min.peek().val > limit) {
+                //Index find kro jha p index jye so dikkat n ho
                 j = Math.min(max.peek().idx, min.peek().idx) + 1;
 
                 while (max.peek().idx < j) {
