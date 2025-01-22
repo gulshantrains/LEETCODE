b30c98f66bf2f2016f4598875e1a2eb14a004f2e
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-01-22 16:04:36 +0530

    Time: 2 ms (82.76%), Space: 44.3 MB (84.46%) - LeetHub

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -0,0 +1,24 @@
+/*
+--You need to ensure that the elements in the array are distinct
+--Remove 3 elements from the beginning of the array. If the array has fewer than 3  elements, remove all remaining elements.
+--Return the minimum number of operations needed to make the elements in the array distinct.
+*/
+class Solution {
+    public int minimumOperations(int[] nums) {
+        // take a frequency array
+        int[] freq = new int[101];
+        Arrays.fill(freq,0);
+
+        // now we will start from back (nums.length-1) as from back we will get get to
+        // ..know KAUN SE ELEMENT TAK REMOVE KRNA MTLB WHA TAK TO ZARUR HE REMOVE KRNA
+        // ..HOGA so we will find kitna count hoga for [n/3]
+
+//count increase kro phle then check kro if we have >1 
+//..i+1 as we are 0 indexed & Math.ceil as we haave to round off to INT
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (++freq[nums[i]] > 1)
+                return (int) Math.ceil((double)(i + 1 )/ 3);
+        }
+        return 0;
+    }
+}
