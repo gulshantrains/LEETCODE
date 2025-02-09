27b0de05994b80f3e09c0706adcf70b01ec9c28e
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-02-09 10:44:34 +0530

    Time: 27 ms (100%), Space: 57.8 MB (59.12%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -1,23 +1,21 @@
+//Whenever SEE EXPRESSION TRY TO RESOLVE IT
 class Solution {
     public long countBadPairs(int[] nums) {
         int n = nums.length;
-        long total = 0; // Total combination Possible
+        long totalgood = 0;
 
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            // Find nums[i]-i 0R nums[j]-j
-            int diff = nums[i] - i;
-            // If we seen that bad pair already
-            int prevcount = map.getOrDefault(diff, 0);
-            total += i - prevcount;
-            map.put(diff, prevcount + 1);
+            //map.merge(val,number,mapping type)
+            totalgood += map.merge(nums[i] - i, 1, Integer::sum) - 1;
         }
-        return total;
+        return 1L*n*(n - 1) / 2 - totalgood;
     }
 }
-/*MY PROBLEM:
--->I WAS UNABLE TO THINK OF TC LESS THAN O(n^2)
--->I WAS UNABLE TO THINK HOW WE CAN FIND IF WE HAVE SEEN ANY GOOD PAIR means 
-.......j-i==nums[j]-nums[i]
--->j - i != nums[j] - nums[i] ---||--- nums[i]-i != nums[j]-j
-*/
+/*
+ * MY PROBLEM:
+ * -->I WAS UNABLE TO THINK OF TC LESS THAN O(n^2)
+ * -->I WAS UNABLE TO THINK HOW WE CAN FIND IF WE HAVE SEEN ANY GOOD PAIR means
+ * .......j-i==nums[j]-nums[i]
+ * -->j - i != nums[j] - nums[i] ---||--- nums[i]-i != nums[j]-j
+ */
