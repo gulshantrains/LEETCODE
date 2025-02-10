3c9efeeae03f570dd67c1ab251d2ea6058b6f667
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-02-10 13:15:07 +0530

    Time: 4 ms (15.23%), Space: 43.7 MB (5.84%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.java
+++ 3447-clear-digits/3447-clear-digits.java
@@ -1,17 +1,19 @@
 class Solution {
     public String clearDigits(String s) {
-        String num = "0123456789";
+        String num = "0123456789"; // To check if x is number or not
         Stack<Character> st = new Stack<>();
         st.push(s.charAt(0));
 
         for (int i = 1; i < s.length(); i++) {
             char x = s.charAt(i);
-            
-            if (!st.isEmpty() && num.contains(String.valueOf(x))) {
+
+            // If stack top element is character and current char is NUMBER then pop top
+            // element
+            if (!st.isEmpty() && num.contains(String.valueOf(x)))
                 st.pop();
-            } else if (!num.contains(String.valueOf(x))) {
-                st.push(x);
-            }
+            else if (!num.contains(String.valueOf(x)))
+                st.push(x);// Push char is it is not a NUMBER
+
         }
         StringBuilder ss = new StringBuilder("");
         while (!st.isEmpty()) {
