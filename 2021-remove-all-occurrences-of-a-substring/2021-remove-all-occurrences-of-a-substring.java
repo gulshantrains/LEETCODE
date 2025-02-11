8ddfe988d1640760f8e36af93d159d4eafca1b33
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-02-11 14:21:21 +0530

    Time: 11 ms (12.17%), Space: 44.4 MB (41.99%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,6 +1,7 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        String ans = s; //New string 
+        String ans = s; //New string for storing answer
+        //If s conatins part then replace it with null and continue this till over
         while (ans.length() > 0 && ans.contains(part)) {
             ans = ans.replaceFirst(part, "");
         }
