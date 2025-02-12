cf4b69c2e8f47bc9d919752b0e3bb139ec8c0c87
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-02-12 20:42:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -7,19 +7,16 @@ class Solution {
             int digitSum = sum(num);
 
             if (maxdig.containsKey(digitSum)) {
-                // Found another number with the same digit sum → Compute max sum
                 maxSum = Math.max(maxSum, num + maxdig.get(digitSum));
 
-                // Update max for this digit sum
-                maxdig.put(digitSum, Math.max(maxdig.get(digitSum), num));
-            } else {
+                if (maxdig.get(digitSum) > num)
+                    maxdig.put(digitSum, num);
+            } else
                 maxdig.put(digitSum, num);
-            }
         }
 
         return maxSum;
     }
-
     private int sum(int num) {
         int s = 0;
         while (num != 0) {
