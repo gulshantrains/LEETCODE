01a75625a4305be63db48edf14b6a7476ed29328
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-02-14 01:01:26 +0530

    Time: 27 ms (29.26%), Space: 57.8 MB (55.43%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -5,20 +5,22 @@
 //If we add some multiple of k then % will be always same
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        if(nums.length<2) return false;
+        if (nums.length < 2)
+            return false;
 
-        HashMap<Integer,Integer> map=new HashMap<>();
-        map.put(0,-1);//For extra use of direct divisibilty
-        int sum=0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);// For extra use of direct divisibilty
+        int sum = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            sum +=nums[i];
-            int val=sum %k;
-            if(map.containsKey(val)){
-                int idx=map.get(val);
-                if(i-idx>=2) return true;
-            }else
-             map.put(val,i);
+            sum += nums[i];
+            int val = sum % k;
+            if (map.containsKey(val)) {
+                int idx = map.get(val);
+                if (i - idx >= 2)
+                    return true;
+            } else
+                map.put(val, i);
         }
         return false;
     }
