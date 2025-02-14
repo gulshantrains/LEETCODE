61b66b198ad4ff2ac03056151f76e3dc8b723a01
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-02-15 00:21:37 +0530

    Time: 16 ms (63.31%), Space: 68.8 MB (74.65%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.java
@@ -1,3 +1,5 @@
+//i was unable to find Optimal solution and also for case of when we get 0 as number
+//whenever 0 comes reset array as uske phle sab 0 he honge 
 class ProductOfNumbers {
     List<Integer> ls = new ArrayList<>();
 
@@ -5,16 +7,24 @@ class ProductOfNumbers {
     }
 
     public void add(int num) {
-        ls.add(num);
+        if (num == 0)
+            ls.clear();
+        else if (ls.size() == 0) {
+            ls.add(num);
+        } else {
+            ls.add(num * ls.get(ls.size() - 1));
+        }
     }
 
     public int getProduct(int k) {
-        int ans = 1;
-        int n = ls.size();
-        for (int i = n - k; i < n; i++) {
-            ans = ans * ls.get(i);
-        }
-        return ans;
+        if (k == ls.size())
+            return ls.get(ls.size() - 1);
+
+        if (k > ls.size())
+            return 0;
+
+        return ls.get(ls.size() - 1) / ls.get(ls.size() - k - 1);
+
     }
 }
 
