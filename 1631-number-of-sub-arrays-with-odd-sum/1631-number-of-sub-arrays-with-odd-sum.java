b0a00d6e2317bbd553c08f2646663ad24219cbb9
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-02-25 17:22:52 +0530

    Time: 5 ms (97.83%), Space: 58 MB (54.66%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
@@ -3,8 +3,7 @@ class Solution {
         int n = arr.length;
         long ans = 0;
         int evencnt = 1, oddcnt = 0; // As pre=0 so evencnt=1 it will help if
-        int pre = 0;
-        long M = 1000000007; // n[i] is odd so single element
+        int pre = 0;                 //.. n[i] is odd so single element
 
         for (int i = 0; i < n; i++) {
             pre += arr[i];
@@ -17,6 +16,6 @@ class Solution {
                 evencnt++;
             }
         }
-        return (int)(ans % M);
+        return (int)(ans % 1000000007);
     }
 }
