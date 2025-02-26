91ad7b959c3c44acfc8e1da82b55861e808ca3ec
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-02-26 17:32:12 +0530

    Time: 11 ms (98.05%), Space: 29.3 MB (10.98%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
@@ -1,5 +1,10 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
+        #Take prefix sum and find diff between max and min 
+        ls=list(accumulate(nums ,initial=0))
+
+        return max(ls)-min(ls)
+        """
         pos_sum = neg_sum = max_sum = min_sum = 0
 
         for x in nums:
@@ -9,4 +14,4 @@ class Solution:
             max_sum = max(max_sum, pos_sum)
             min_sum = min(min_sum, neg_sum)
 
-        return max(max_sum, abs(min_sum))
+        return max(max_sum, abs(min_sum)) """
