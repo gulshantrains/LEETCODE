6bbeb3056d0463ae9cf2cf78252c5b426b2c5ffa
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-03-05 18:10:05 +0530

    Time: 0 ms (100%), Space: 40.9 MB (16.39%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,13 +1,6 @@
-/*
-n=1-->1    1+0 
-n=2-->5    4+1
-n=3-->13   9+4
-n=4-->25   16+9
-...
-n=n     n^2 + (n-1)^2 PATTERN AND WE CALL THIS DIAMOND PATTERn
-*/
 class Solution {
     public long coloredCells(int n) {
-        return (long)n*n + (long)(n-1)*(n-1);
+        // 2n^2 - 2n + 1.
+        return 2L * n * n - 2L * n + 1;
     }
 }
