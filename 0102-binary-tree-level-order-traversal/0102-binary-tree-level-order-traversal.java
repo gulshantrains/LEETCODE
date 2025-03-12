ff289df328a3e878172f56d7c4744e371c3ec59f
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-03-12 23:24:28 +0530

    Time: 1 ms (89.59%), Space: 44.7 MB (97.69%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,40 +1,43 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-       List<List<Integer>> ans=new ArrayList<>();
-       Queue<TreeNode> que=new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> que = new LinkedList<>();
 
-       if(root==null) return ans;
-       que.add(root);
+        if (root == null)
+            return ans;
+        que.add(root);
 
-       while(!que.isEmpty()){
-        int n=que.size();
-        List<Integer> ls=new ArrayList<>();
+        while (!que.isEmpty()) {
+            int n = que.size();
+            List<Integer> ls = new ArrayList<>(n);
 
-        while(n-->0){
-            TreeNode temp=que.poll();
+            while (n-- > 0) {
+                TreeNode temp = que.poll();
 
-            ls.add(temp.val);
+                ls.add(temp.val);
 
-            if(temp.left != null) que.add(temp.left);
-            if(temp.right != null) que.add(temp.right);
+                if (temp.left != null)
+                    que.add(temp.left);
+                if (temp.right != null)
+                    que.add(temp.right);
+            }
+            ans.add(ls);
         }
-        ans.add(ls);
-       } 
-       return ans;
+        return ans;
     }
 }
