90bd80e486ba52aaafdd6b7cd5d3d1073fbd21c9
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-03-17 00:25:00 +0530

    Time: 1061 ms (30.41%), Space: 22 MB (49.42%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -1,16 +1,21 @@
 class Solution:
+    def can(self, ranks: List[int], time: int, cars: int) -> bool:
+        total = 0
+        for x in ranks:
+            total += int(sqrt(time // x))
+
+        return total >= cars
+
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        l,r,ans=1,max(ranks)*cars*cars,0
+        l, r, ans = 1, max(ranks) * cars * cars, 0
 
-        while(l<=r):
-            mid=(l+r)//2
-            total = sum(int(sqrt(mid // num)) for num in ranks)
+        while l <= r:
+            mid = (l + r) // 2
 
-            if(total>=cars):
-                ans=mid
-                r=mid-1
+            if self.can(ranks, mid, cars):
+                ans = mid
+                r = mid - 1
             else:
-                l=mid+1
+                l = mid + 1
 
         return ans
-        
