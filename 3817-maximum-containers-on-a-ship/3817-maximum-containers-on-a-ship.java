635b61cd9cdbcf25fa0c56013ba427165264fde9
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-03-24 04:31:48 +0530

    Time: 0 ms (100%), Space: 41.1 MB (100%) - LeetHub

--- 3817-maximum-containers-on-a-ship/3817-maximum-containers-on-a-ship.java
+++ 3817-maximum-containers-on-a-ship/3817-maximum-containers-on-a-ship.java
@@ -1,9 +1,5 @@
 class Solution {
     public int maxContainers(int n, int w, int maxWeight) {
-        int total=maxWeight / w; //Total How many containers are possible
-        int org=n*n*w; //Can load how many
-
-        if(org<maxWeight) return n*n;
-        return total;
+        return (n * n * w < maxWeight ? n * n : maxWeight / w);
     }
 }
