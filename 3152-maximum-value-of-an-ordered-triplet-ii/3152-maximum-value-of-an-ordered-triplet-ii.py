0c7d0f9488ac7e097ea740589154af645e245c99
Gulshan Kumar <137637995+flyingelephhant@users.noreply.github.com>
2025-04-03 03:19:07 +0530

    Time: 192 ms (50.17%), Space: 29.4 MB (87.03%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.py
@@ -1,28 +1,14 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n < 3:
-            return 0
+        res=0
+        pre_max=nums[0]
+        max_diff=0
 
-        lmax = [0]*n
-        rmax = [0] * n
+        for i in range(1,len(nums)):
+            res=max(res,max_diff*nums[i])
+            pre_max=max(pre_max,nums[i])
+            max_diff=max(max_diff,pre_max-nums[i])
 
-        lmax[0] = rmax[n - 1] = 0
-
-        # For index 1, the maximum on the left is simply nums[0].
-        lmax[1] = nums[0]
-        # For index n-2, the maximum on the right is simply nums[n-1].
-        rmax[n - 2] = nums[n - 1]
-
-        for i in range(2, n):
-            lmax[i] = max(lmax[i - 1], nums[i - 1])
-
-        for i in range(n - 3, -1, -1):
-            rmax[i] = max(rmax[i + 1], nums[i + 1])
-
-        ans = 0
-        for i in range(1, n - 1):
-            candidate = (lmax[i] - nums[i]) * rmax[i]
-            ans = max(ans, candidate)
-
-        return ans
+        return res
+         
+        
