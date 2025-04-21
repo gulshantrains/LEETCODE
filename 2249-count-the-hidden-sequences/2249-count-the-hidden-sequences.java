d741b0ac32faacaf1d1ca7b11ef622ecbd3052bd
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-04-21 05:46:56 +0530

    Time: 4 ms (92.06%), Space: 59.2 MB (93.65%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.java
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.java
@@ -2,8 +2,8 @@ class Solution {
     public int numberOfArrays(int[] differences, int lower, int upper) {
         long sum = 0, min=0,max=0 ;   // prefix sum
             
-        for (var diff : differences) {
-            sum += diff;           // update sum
+        for (var x : differences) {
+            sum += x;           // update sum
             min = Math.min(min, sum);    // update min
             max = Math.max(max, sum);   // update max
         }
