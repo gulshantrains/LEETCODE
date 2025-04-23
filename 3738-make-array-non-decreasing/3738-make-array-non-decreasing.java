debc2f409d550dc58b0b77eda86aaccb657137fc
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-04-23 21:22:18 +0530

    Time: 3 ms (63.64%), Space: 73.3 MB (31.05%) - LeetHub

--- 3738-make-array-non-decreasing/3738-make-array-non-decreasing.java
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maximumPossibleSize(int[] nums) {
-        int ans = 0, prevmax = 0;
+        int ans = 0, prevmax = Integer.MIN_VALUE;
 
         for (var x : nums) {
             if (x >= prevmax) {
