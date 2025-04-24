dbaeca2bd7ec9463afeb3e7e5aef338fbaedc9b0
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-04-24 19:44:13 +0530

    Time: 196 ms (24.47%), Space: 45.3 MB (23.52%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -1,23 +1,24 @@
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
         int n = nums.length;
-        int ans = 0;
+        int ans = 0, d = 0;
 
-        Map<Integer, Integer> mp = new HashMap<>();
-
-        for (int x : nums)
-            mp.put(x, mp.getOrDefault(x, 0) + 1);
-
-        int d = mp.size(); //Total Distinct element
+        boolean[] exist = new boolean[2001];
+        for (var x : nums) {
+            if (!exist[x]) {
+                exist[x] = true;
+                d++;
+            }
+        }
+        HashSet<Integer> ch = new HashSet<>();
 
         for (int i = 0; i < n; i++) {
-            Map<Integer, Integer> temp = new HashMap<>(); // fresh map for each i
+            ch.clear();
+
             for (int j = i; j < n; j++) {
-                temp.put(nums[j], temp.getOrDefault(nums[j], 0) + 1);
-                if (temp.size() == d)
+                ch.add(nums[j]);
+                if (ch.size() == d)
                     ans++;
-                else if (temp.size() > d)
-                    break; // no need to continue if distinct count exceeded
             }
         }
         return ans;
