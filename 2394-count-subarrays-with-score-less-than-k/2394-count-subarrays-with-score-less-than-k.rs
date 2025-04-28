1c87df9ead63d7cf66ad5aee255126aaef125e7a
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-04-28 23:10:04 +0530

    Time: 3 ms (80%), Space: 3.3 MB (80%) - LeetHub

---
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn count_subarrays(nums: Vec<i32>, k: i64) -> i64 {
+        let n = nums.len();
+        let mut res = 0;
+        let mut total = 0;
+        let mut i = 0;
+        for j in 0..n {
+            total += nums[j] as i64;
+            while i <= j && total * (j - i + 1) as i64 >= k {
+                total -= nums[i] as i64;
+                i += 1;
+            }
+            res += j - i + 1;
+        }
+        res as i64
+    }
+}
