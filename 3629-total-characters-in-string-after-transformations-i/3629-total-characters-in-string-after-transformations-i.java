03ddd6186e53f665aa9f3396c805b24647b3c133
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-05-13 17:16:37 +0530

    Time: 147 ms (48.1%), Space: 45.8 MB (41.77%) - LeetHub

---
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.java
@@ -0,0 +1,33 @@
+class Solution {
+    public static int mod = 1000000007;
+
+    public int lengthAfterTransformations(String s, int t) {
+        int[] map = new int[26];
+
+        for (var x : s.toCharArray())
+            map[x - 'a']++;
+
+        while (t-- > 0) {
+            int[] temp = new int[26];
+
+            for (int i = 0; i < 26; i++) {
+                if (map[i] == 0)
+                    continue;
+
+                if (i == 25) {
+                    temp[0] = (temp[0] + map[25]) % mod;
+                    temp[1] = (map[25] + temp[1]) % mod;
+                } else {
+                    temp[i + 1] += (map[i] + temp[i + 1]) % mod;
+                }
+            }
+            map = temp;
+
+        }
+        int ans = 0;
+        for (var xx : map)
+            ans = (ans + xx) % mod;
+
+        return ans % mod;
+    }
+}
