a89ede6d49a172e5c05e3c2b4acc1f185bc227a6
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-05-20 08:40:47 +0530

    Time: 3 ms (79.52%), Space: 96.3 MB (8.89%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -1,20 +1,20 @@
 class Solution {
-    public boolean isZeroArray(int[] nums, int[][] queries) {
+    public boolean isZeroArray(int[] nums, int[][] qr) {
         int n = nums.length;
-        int[] pre = new int[n + 1];
+        int[] dif = new int[n + 1];
 
-        for (var x : queries) {
-            pre[x[0]] += 1;
-            pre[x[1] + 1] -= 1;
+        for (var x : qr) {
+            dif[x[0]]++;
+            dif[x[1] + 1]--;
         }
-
-        for (int i = 1; i < n + 1; i++) {
-            pre[i] += pre[i - 1];
+        for(int i=0; i<n; i++){
+            dif[i+1] +=dif[i];
         }
-        
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
+            if(dif[i] == nums[i]) continue;
             if(nums[i]==0) continue;
-            if(nums[i] >pre[i]) return false;
+            if(dif[i]>nums[i]) continue;
+            if(dif[i]<nums[i]) return false;
         }
         return true;
     }
