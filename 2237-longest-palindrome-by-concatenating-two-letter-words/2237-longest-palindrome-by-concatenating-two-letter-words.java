4b774085d130889820bf2d24686caf7f8634f491
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-05-25 17:55:27 +0530

    Time: 61 ms (58.13%), Space: 57.8 MB (69.92%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,49 +1,47 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        // Create a 2D array to store counts of words.
-        // Index [0][0] corresponds to "aa", [0][1] to "ab", ..., [25][25] to "zz".
-        int[][] counts = new int[26][26];
+        Map<String, Integer> mp = new HashMap<>();
 
-        // Populate the counts array
         for (String word : words) {
-            int char1 = word.charAt(0) - 'a'; 
-            int char2 = word.charAt(1) - 'a'; 
-            counts[char1][char2]++;
+            mp.put(word, mp.getOrDefault(word, 0) + 1); // Put all in map
         }
 
         int ans = 0;
-        boolean hasCenter = false; 
+        boolean hasCenter = false; // Flag to check if we have a single palindromic word for the center
 
-        for (int i = 0; i < 26; i++) {
-            for (int j = 0; j < 26; j++) {
-                // Current word represented by (char)('a' + i) + (char)('a' + j)
+        for (String word : new HashMap<>(mp).keySet()) {
+            String temp = word;
+            String rev = "" + temp.charAt(1) + temp.charAt(0);
 
-                // Case 1: Palindromic words like "gg", "aa", etc. (i == j)
-                if (i == j) {
-                    int count = counts[i][j];
-                    // Each pair contributes 4 to the length
-                    ans += (count / 2) * 4;
-                    // If there's an odd number, one can be the center
-                    if (count % 2 == 1) {
-                        hasCenter = true;
-                    }
+            if (temp.equals(rev)) { // It's a palindromic word like "gg", "aa", etc.
+                int cnt = mp.get(temp);
+
+                ans += (cnt / 2) * 4;
+
+                if (cnt % 2 == 1) {
+                    hasCenter = true;
                 }
-                // Case 2: Symmetric pairs like "lc" and "cl" (i != j)
-                // To avoid double-counting, only process if i < j.
-                // If we process (i, j), its reverse (j, i) will be handled by this logic.
-                else if (i < j) {
-                    int countWord = counts[i][j]; // Count of word "ij"
-                    int countRev = counts[j][i];  // Count of word "ji" (reverse)
+            } else if (mp.containsKey(rev)) { // It's a symmetric pair like "lc" and "cl"
+                int cntTemp = mp.get(temp);
+                int cntRev = mp.get(rev);
 
-                    // The number of pairs we can form is limited by the minimum count
-                    int pairs = Math.min(countWord, countRev);
-                    ans += pairs * 4; // Each pair contributes 4 (e.g., "lc" + "cl" = "lccl")
-                }
+                int pairs = Math.min(cntTemp, cntRev); // Number of pairs we can form
+
+                ans += pairs * 4; // Each pair contributes 4 (e.g., "lc" + "cl" -> "lccl")
+
+                mp.put(rev, pairs - mp.get(rev));
+                if (mp.get(rev) <= 0)
+                    mp.remove(rev);
+
+                mp.put(temp, pairs - mp.get(temp));
+                if (mp.get(temp) <= 0)
+                    mp.remove(temp);
+
             }
         }
 
         if (hasCenter) {
-            ans += 2;
+            ans += 2; // Add 2 for the single palindromic word in the center
         }
 
         return ans;
