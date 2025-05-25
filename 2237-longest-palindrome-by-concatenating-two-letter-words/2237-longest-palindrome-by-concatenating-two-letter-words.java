610aa8ac554d374edb031218732b4823568aff64
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-05-25 18:11:21 +0530

    Time: 54 ms (83.74%), Space: 58.2 MB (61.59%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,49 +1,35 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        Map<String, Integer> mp = new HashMap<>();
-
-        for (String word : words) {
-            mp.put(word, mp.getOrDefault(word, 0) + 1); // Put all in map
+        Map<String, Integer> mpp = new HashMap<>();
+        for (var w : words) {
+            mpp.put(w, mpp.getOrDefault(w, 0) + 1);
         }
 
-        int ans = 0;
-        boolean hasCenter = false; // Flag to check if we have a single palindromic word for the center
+        int totalLength = 0;
+        int flag = 0;
 
-        for (String word : new HashMap<>(mp).keySet()) {
-            String temp = word;
-            String rev = "" + temp.charAt(1) + temp.charAt(0);
+        for (var x : mpp.entrySet()) {
+            String word = x.getKey();
+            int freq = x.getValue();
 
-            if (temp.equals(rev)) { // It's a palindromic word like "gg", "aa", etc.
-                int cnt = mp.get(temp);
+            String rev = new StringBuilder(word).reverse().toString();
 
-                ans += (cnt / 2) * 4;
+            if (word.equals(rev)) {
+                totalLength += (freq / 2) * 4;
 
-                if (cnt % 2 == 1) {
-                    hasCenter = true;
+                if (freq % 2 == 1) {
+                    flag = 1;
                 }
-            } else if (mp.containsKey(rev)) { // It's a symmetric pair like "lc" and "cl"
-                int cntTemp = mp.get(temp);
-                int cntRev = mp.get(rev);
+            } else {
+                if (word.compareTo(rev) < 0 && mpp.containsKey(rev)) {
+                    int freqOfReversed = mpp.get(rev);
+                    int numberOfPairs = Math.min(freq, freqOfReversed);
 
-                int pairs = Math.min(cntTemp, cntRev); // Number of pairs we can form
-
-                ans += pairs * 4; // Each pair contributes 4 (e.g., "lc" + "cl" -> "lccl")
-
-                mp.put(rev, pairs - mp.get(rev));
-                if (mp.get(rev) <= 0)
-                    mp.remove(rev);
-
-                mp.put(temp, pairs - mp.get(temp));
-                if (mp.get(temp) <= 0)
-                    mp.remove(temp);
-
+                    totalLength += numberOfPairs * 4;
+                }
             }
         }
 
-        if (hasCenter) {
-            ans += 2; // Add 2 for the single palindromic word in the center
-        }
-
-        return ans;
+        return totalLength + flag * 2;
     }
 }
