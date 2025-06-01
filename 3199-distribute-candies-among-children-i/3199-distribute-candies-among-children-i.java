1d22fe86a2e584de88fcd17bc4895d6ff87a118c
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-01 15:59:22 +0530

    Time: 1 ms (93.52%), Space: 40.8 MB (62.96%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.java
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.java
@@ -2,12 +2,9 @@ class Solution {
     public int distributeCandies(int n, int limit) {
         int cnt = 0;
         for (int i = 0; i <= limit; i++) {
-            int x = i;
-
             for (int j = 0; j <= limit; j++) {
-                int y = j;
 
-                int z = (n - x - y);
+                int z = (n - i - j);
 
                 if (z >= 0 && z <= limit)
                     cnt++;
