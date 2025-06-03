29824c3cd254941bb96a0ffca04c48e5d0ebc34b
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-03 16:27:34 +0530

    Time: 15 ms (78.55%), Space: 44.8 MB (91.37%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        int n = num.length();
+        if (n == k || k > n)
+            return "0";
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            int val = num.charAt(i) - '0';
+
+            //We want Smallest digit on Least Significiant Place so removr if we have seen smaller number and also k>0
+            while (!st.isEmpty() && k > 0 && st.peek() > val) {
+                st.pop();
+                k--;
+            }
+            st.push(val);
+        }
+    //When Number is Increasing 1,2,3,4,5,6,7 then we have to remove from top
+        while (!st.isEmpty() && k > 0) {
+            st.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int x : st) //Enhanced loop to track bottom to top
+            sb.append(x);
+
+        while (sb.length() > 1 && sb.charAt(0) == '0')
+            sb.deleteCharAt(0);
+
+        return sb.length() == 0 ? "0" : sb.toString();
+
+    }
+}
