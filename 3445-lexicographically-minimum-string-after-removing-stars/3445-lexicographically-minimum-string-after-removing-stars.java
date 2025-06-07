123c8dd6ee144cd7e3ec52872ebb0b785d225a57
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-07 14:52:55 +0530

    Time: 193 ms (56.97%), Space: 49 MB (46.67%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
@@ -1,4 +1,5 @@
 class Solution {
+
     class Pair {
         char ch;
         int idx;
@@ -15,38 +16,31 @@ class Solution {
         Queue<Pair> pq = new PriorityQueue<>((a, b) -> {
             if (a.ch != b.ch)
                 return a.ch - b.ch;
-            else
-                return b.idx - a.idx;
+            return b.idx - a.idx;
         });
 
         for (int i = 0; i < n; i++) {
-            char temp = s.charAt(i);
-
-            if (temp == '*' && pq.isEmpty())
-                continue;
-            else if (temp != '*') {
-                pq.offer(new Pair(temp, i));
+            char ch = s.charAt(i);
+            if (ch == '*') {
+                if (!pq.isEmpty())
+                    pq.poll();
             } else {
-                pq.poll();
+                pq.offer(new Pair(ch, i));
             }
         }
-        if (pq.isEmpty())
-            return "";
 
-        ArrayList<Integer> ls = new ArrayList<>();
-
+        // Extract indices and sort
+        List<Integer> ls = new ArrayList<>();
         while (!pq.isEmpty()) {
-            Pair temp = pq.poll();
-            ls.add(temp.idx);
+            ls.add(pq.poll().idx);
         }
         Collections.sort(ls);
 
-        StringBuilder ans = new StringBuilder();
-
-        for (int i = 0; i < ls.size(); i++) {
-            ans.append(s.charAt(ls.get(i)));
+        StringBuilder sb = new StringBuilder();
+        for (int idx : ls) {
+            sb.append(s.charAt(idx));
         }
 
-        return ans.toString();
+        return sb.toString();
     }
 }
