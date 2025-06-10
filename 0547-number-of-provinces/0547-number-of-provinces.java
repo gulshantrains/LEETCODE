5fbfcaa3511e2d19dc8553fb83b2d860d89d0405
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-10 18:29:35 +0530

    Time: 0 ms (100%), Space: 48.5 MB (5.05%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,40 +1,24 @@
 class Solution {
-    private static void dfs(int n, boolean[] visited, ArrayList<ArrayList<Integer>> adj) {
+    private static void dfs(int n, boolean[] visited, int[][] M) {
         visited[n] = true; //Visited
 
-        for (int v : adj.get(n)) {
-
-            if (!visited[v]) {
-                dfs(v, visited, adj);
+        for(int i=0; i<M.length; i++){
+            if(!visited[i] && M[n][i]==1){
+                dfs(i,visited,M);
             }
         }
     }
 
-    public int findCircleNum(int[][] isConnected) {
+    public int findCircleNum(int[][] M) {
         int ans = 0; //It will count same province
-        int V = isConnected.length; //Total town in city
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int V = M.length; //Total town in city
 
-        for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<Integer>());
-        }
-
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if (i == j)
-                    continue;
-
-                if (isConnected[i][j] == 1) {
-                    adj.get(i).add(j);
-                }
-            }
-        }
         boolean[] visited = new boolean[V];
 
         for (int i = 0; i < V; i++) {
             if (!visited[i]) {
-                ans++; //Means a new Province is found
-                dfs(i, visited, adj);
+                ans++; //Means a new Province is found because all connected province
+                dfs(i, visited, M); //is marked in dfs call
             }
         }
         return ans;
