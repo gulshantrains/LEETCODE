08741e50fc085abd6d545e15331d94513d47cb17
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-13 20:46:56 +0530

    Time: 0 ms (100%), Space: 45.6 MB (12.15%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,42 +1,28 @@
 class Solution {
-    // 0->Not visited 1->Red 2->Green
-    public List<List<Integer>> adj;
+    public boolean dfs(int n, int currcolor, int[] colour, int[][] graph) {
+        colour[n] = currcolor;
+        int ncolor = (currcolor == 0) ? 1 : 0;
 
-    public boolean dfs(int n, int[] check, int currcolour) {
-        check[n] = currcolour; //Fill with current colour
-
-        int nextcolour = (currcolour == 1) ? 2 : 1; //What should be adjacent colour
-
-        for (int x : adj.get(n)) {
-            if (check[x] == 0) { //Not visited earlier kavi v
-                if (!dfs(x, check, nextcolour))
+        for (int x : graph[n]) {
+            if (colour[x] == -1) {
+                if (!dfs(x, ncolor, colour, graph))
                     return false;
-            } else if (check[x] == currcolour) //Adjacent Node has same colour no Bipartite
-                return false; 
-            else if (check[x] == nextcolour) //No issue different colour
-                continue;
+            } else if (colour[x] == currcolor)
+                return false;
 
         }
         return true;
     }
 
     public boolean isBipartite(int[][] graph) {
-        adj = new ArrayList<>();
-
-        for (int i = 0; i < graph.length; i++) {
-            adj.add(new ArrayList<>());
-
-            for (int j = 0; j < graph[i].length; j++) {
-                adj.get(i).add(graph[i][j]);
-            }
-        }
         int n = graph.length;
-        int[] check = new int[n];
-        Arrays.fill(check, 0);
+
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
 
         for (int i = 0; i < n; i++) {
-            if (check[i] == 0) {
-                if (!dfs(i, check, 1))
+            if (color[i] == -1) {
+                if (!dfs(i, 0, color, graph))
                     return false;
             }
         }
