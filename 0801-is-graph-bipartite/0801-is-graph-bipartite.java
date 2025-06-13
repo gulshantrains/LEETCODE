e32d7a96a9c2c0811595c24cf7879f0e737b2966
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-13 20:47:15 +0530

    Time: 0 ms (100%), Space: 45.6 MB (6.87%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -4,28 +4,32 @@ class Solution {
         int ncolor = (currcolor == 0) ? 1 : 0;
 
         for (int x : graph[n]) {
-            if (colour[x] == -1) {
-                if (!dfs(x, ncolor, colour, graph))
-                    return false;
-            } else if (colour[x] == currcolor)
-                return false;
-
+            if (colour[x] == -1) { // Case 1: Neighbor is unvisited
+                if (!dfs(x, ncolor, colour, graph)) { // Recurse with alternate color
+                    return false; // Propagate conflict
+                }
+            } else if (colour[x] == currcolor) { // Case 2: Neighbor is visited AND has the SAME color
+                // This is the critical check for non-bipartiteness (odd-length cycle)
+                return false; // Conflict found
+            }
+            // Case 3: Neighbor is visited and has the DIFFERENT color (colour[x] == ncolor). This is valid.
         }
-        return true;
+        return true; // No conflict found in this branch
     }
 
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
 
         int[] color = new int[n];
-        Arrays.fill(color, -1);
+        Arrays.fill(color, -1); // Initialize all nodes as unvisited
 
         for (int i = 0; i < n; i++) {
-            if (color[i] == -1) {
-                if (!dfs(i, 0, color, graph))
-                    return false;
+            if (color[i] == -1) { // If the node hasn't been visited (start of a new component)
+                if (!dfs(i, 0, color, graph)) { // Start DFS with color 0
+                    return false; // If DFS returns false, a conflict was found
+                }
             }
         }
-        return true;
+        return true; // If all components are successfully colored, the graph is bipartite
     }
 }
