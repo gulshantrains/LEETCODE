7118087c75a162025489d3fbaee1913d2bf1a08d
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-14 20:10:13 +0530

    Time: 1 ms (100%), Space: 42.9 MB (22.28%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -8,7 +8,8 @@ class Solution {
         }
     }
 
-    private int[] dir = new int[] { -1, 0, 1, 0, -1 };
+    private int[] dr = new int[] { 0, 0, -1, 1 }; //It will affect row
+    private int[] dc = new int[] { -1, 1, 0, 0 }; //It will affect column
 
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
@@ -16,29 +17,29 @@ class Solution {
 
         Queue<Pair> q = new LinkedList<>();
 
-        int good = 0;
+        int good = 0; //Good Oranges
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
-                    q.offer(new Pair(i, j));
+                    q.offer(new Pair(i, j)); //Fill out all Rotton Oranges
                 } else if (grid[i][j] == 1)
                     good++;
             }
         }
-        if (good == 0)
+        if (good == 0) //No Good oranges already
             return 0;
 
         int time = 0;
         while (good > 0 && !q.isEmpty()) {
 
             int s = q.size();
-            for (int i = 0; i < s; i++) {
+            for (int i = 0; i < s; i++) { //It will Process all batch at once and in Same time frame
                 Pair t = q.poll();
 
                 for (int d = 0; d < 4; d++) {
-                    int nrow = t.row + dir[d];
-                    int ncol = t.col + dir[d + 1];
+                    int nrow = t.row + dr[d];
+                    int ncol = t.col + dc[d];
 
                     if (nrow < n && nrow >= 0 && ncol < m && ncol >= 0 && grid[nrow][ncol] == 1) {
                         grid[nrow][ncol] = 2;
@@ -47,7 +48,7 @@ class Solution {
                     }
                 }
             }
-            time++;
+            time++; //Increase time after processing one batch of queue
         }
         return (good == 0) ? time : -1;
     }
