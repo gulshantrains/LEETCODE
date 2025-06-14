4ff9d08968d24264d372e4d92822cd64aa01c292
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-14 17:21:24 +0530

    Time: 2 ms (32.9%), Space: 45 MB (65.62%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,35 +1,45 @@
 class Solution {
-    public boolean dfs(int n, int currcolor, int[] colour, int[][] graph) {
-        colour[n] = currcolor;
-        int ncolor = (currcolor == 0) ? 1 : 0;
+    class Pair {
+        int val;
+        int color;
 
-        for (int x : graph[n]) {
-            if (colour[x] == -1) { // Case 1: Neighbor is unvisited
-                if (!dfs(x, ncolor, colour, graph)) { // Recurse with alternate color
-                    return false; // Propagate conflict
-                }
-            } else if (colour[x] == currcolor) { // Case 2: Neighbor is visited AND has the SAME color
-                // This is the critical check for non-bipartiteness (odd-length cycle)
-                return false; // Conflict found
-            }
-            // Case 3: Neighbor is visited and has the DIFFERENT color (colour[x] == ncolor). This is valid.
+        Pair(int val, int color) {
+            this.val = val;
+            this.color = color;
         }
-        return true; // No conflict found in this branch
     }
 
     public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
+        int V = graph.length;
+        int[] color = new int[V];
+        Arrays.fill(color, -1);
 
-        int[] color = new int[n];
-        Arrays.fill(color, -1); // Initialize all nodes as unvisited
+        Queue<Pair> qe = new LinkedList<>();
 
-        for (int i = 0; i < n; i++) {
-            if (color[i] == -1) { // If the node hasn't been visited (start of a new component)
-                if (!dfs(i, 0, color, graph)) { // Start DFS with color 0
-                    return false; // If DFS returns false, a conflict was found
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                qe.offer(new Pair(i, 0));
+                color[i] = 0;
+
+                while (!qe.isEmpty()) {
+                    Pair x = qe.poll();
+
+                    int v = x.val;
+                    int c = x.color;
+
+                    for (int k : graph[v]) {
+                        if (color[k] == -1) {
+                            color[k] = 1 - c;
+
+                            qe.offer(new Pair(k, 1 - c));
+                        } else if (color[k] == c) {
+                            return false;
+                        }
+                    }
                 }
             }
+
         }
-        return true; // If all components are successfully colored, the graph is bipartite
+        return true;
     }
 }
