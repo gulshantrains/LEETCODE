6af92eac246d6a92500a4b31a60a773bead224c6
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-15 17:46:03 +0530

    Time: 1 ms (92.38%), Space: 40.4 MB (94.29%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.java
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.java
@@ -1,40 +1,38 @@
 class Solution {
 
     public int maxDiff(int num) {
-        String temp1 = num + ""; //Integer converted into string
-        int i = 0;
+        String numStr = String.valueOf(num);
 
-        //Take pointer to place where first digit is not 9
-        while (i < temp1.length() && (temp1.charAt(i) == '9')) {
-            i++;
-        }
-        System.out.println("idx: " + i);
-        //Make maximum number by replacing it with 9
-        if (i < temp1.length()) {
-            char x1 = temp1.charAt(i);
-            char y1 = '9';
-
-            temp1 = temp1.replace(x1, y1);
-        }
-        int a = Integer.valueOf(temp1);
-        System.out.println("Num a: " + a);
-
-        String temp2 = num + ""; //Integer converted into string
-        i = 0;
-
-        if (temp2.charAt(0) != '1') {
-            temp2 = temp2.replace(temp2.charAt(0), '1');
-
-        } else {
-            while (i < temp2.length() && (temp2.charAt(i) == '0' || temp2.charAt(i) == '1')) {
-                i++;
+        // Step 1: Create the maximum number by replacing the first non-'9' digit with '9'
+        String maxStr = numStr;
+        for (char c : numStr.toCharArray()) {
+            if (c != '9') {
+                maxStr = numStr.replace(c, '9');
+                break;
             }
-            if (i < temp2.length())
-                temp2 = temp2.replace(temp2.charAt(i), '0');
         }
+        int maxNum = Integer.parseInt(maxStr);
 
-        int b = Integer.valueOf(temp2);
-        return a - b;
+        // Step 2: Create the minimum number by replacing
+        // - the first digit (if not '1') with '1', or
+        // - the first non-'0'/'1' digit (after first) with '0'
+        String minStr = numStr;
+        char firstChar = numStr.charAt(0);
 
+        if (firstChar != '1') {
+            minStr = numStr.replace(firstChar, '1');
+        } else {
+            for (int i = 1; i < numStr.length(); i++) {
+                char c = numStr.charAt(i);
+                if (c != '0' && c != '1') {
+                    minStr = numStr.replace(c, '0');
+                    break;
+                }
+            }
+        }
+        int minNum = Integer.parseInt(minStr);
+
+        // Return the difference between maximum and minimum numbers
+        return maxNum - minNum;
     }
 }
