aed20ffe543890a944b2f6048289898ecfcf645c
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-16 23:49:38 +0530

    Time: 6 ms (98.92%), Space: 56.7 MB (53.61%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -1,9 +1,6 @@
 class Solution {
     private void dfs(int[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length)
-            return;
-
-        if (grid[i][j] == 0)
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0)
             return;
 
         grid[i][j] = 0;
