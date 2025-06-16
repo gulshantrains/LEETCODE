8c8b849cab1e8990c36d184c7c0de39367878213
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-17 00:00:55 +0530

    Time: 7 ms (87.72%), Space: 56 MB (63.34%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -15,19 +15,11 @@ class Solution {
         int n = grid.length; //Row
         int m = grid[0].length; //Column
 
-        for (int i = 0; i < m; i++) {
-            if (grid[0][i] == 1)
-                dfs(grid, 0, i); //First Row 
-
-            if (grid[n - 1][i] == 1)
-                dfs(grid, n - 1, i); //Last Row
-        }
-        for (int i = 1; i < n - 1; i++) {
-            if (grid[i][0] == 1)
-                dfs(grid, i, 0); //First column
-
-            if (grid[i][m - 1] == 1) //Last column
-                dfs(grid, i, m - 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == m - 1)
+                    dfs(grid, i, j);
+            }
         }
         int ans = 0;
 
@@ -38,5 +30,6 @@ class Solution {
             }
         }
         return ans;
+        // return Arrays.stream(grid).mapToInt(row-> Arrays.stream(row).sum()).sum();
     }
 }
