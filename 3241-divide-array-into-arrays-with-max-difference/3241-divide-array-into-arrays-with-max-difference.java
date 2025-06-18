3c33ec3c418a89386071d3fa53db3478f05af2c6
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-18 23:58:17 +0530

    Time: 21 ms (95.81%), Space: 59.4 MB (75.92%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
@@ -1,23 +1,32 @@
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
         int n = nums.length;
-        int[][] ans = new int[n / 3][3];
+        int[][] ans = new int[n / 3][];
 
         Arrays.sort(nums);
 
         for (int i = 0; i < n; i += 3) {
-            int a = nums[i], b = nums[i + 1], c = nums[i + 2];
+            int[] temp = Arrays.copyOfRange(nums, i, i + 3);
 
-            int maxV = Math.max(a, Math.max(b, c));
-            int minV = Math.min(a, Math.min(b, c));
-
-            if (maxV - minV > k)
+            if (temp[2] - temp[0] > k)
                 return new int[][] {};
 
-            ans[i / 3][0] = a;
-            ans[i / 3][1] = b;
-            ans[i / 3][2] = c;
+            ans[i / 3] = temp;
         }
+
         return ans;
     }
 }
+// for (int i = 0; i < n; i += 3) {
+//     int a = nums[i], b = nums[i + 1], c = nums[i + 2];
+
+//     int maxV = Math.max(a, Math.max(b, c));
+//     int minV = Math.min(a, Math.min(b, c));
+
+//     if (maxV - minV > k)
+//         return new int[][] {};
+
+//     ans[i / 3][0] = a;
+//     ans[i / 3][1] = b;
+//     ans[i / 3][2] = c;
+// }
