8f82ea4cd625509262667bc3d30dc93eba27db40
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-21 17:28:31 +0530

    Time: 4 ms (100%), Space: 45.6 MB (68.09%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.java
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.java
@@ -1,9 +1,8 @@
 class Solution {
     public int minimumDeletions(String word, int k) {
-        int[] freq = new int[26];
-
-        for (char ch : word.toCharArray()) {
-            freq[ch - 'a']++;
+        int [] freq = new int[26];
+        for(int i =0;i<word.length();i++){
+            freq[word.charAt(i)-'a']++;
         }
 
         int ans = Integer.MAX_VALUE; // Initialize answer for minimization
