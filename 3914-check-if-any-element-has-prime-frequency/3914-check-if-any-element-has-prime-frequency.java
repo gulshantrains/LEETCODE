d133910888be7fd8f020b15cc589681fe62abd60
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-06-22 21:28:30 +0530

    Time: 1 ms (98.7%), Space: 42.6 MB (97.31%) - LeetHub

---
+++ 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.java
@@ -0,0 +1,29 @@
+class Solution {
+    private boolean isPrime(int n) {
+        if (n == 2)
+            return true;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean checkPrimeFrequency(int[] nums) {
+        int[] freq = new int[101];
+        Arrays.fill(freq, 0);
+
+        for (var x : nums) {
+            freq[x]++;
+        }
+
+        for (int x : freq) {
+            if (x <= 1)
+                continue;
+
+            if (isPrime(x))
+                return true;
+        }
+        return false;
+    }
+}
