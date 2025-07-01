564802ff3fba66211e33017778a42e10626c9475
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-07-01 17:19:53 +0530

    Time: 3 ms (99.24%), Space: 61.5 MB (49.08%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
@@ -3,6 +3,7 @@ class Solution {
     public static int[] rank;
 
     public int find(int x) {
+        // If x is the parent of itself, then it is the root of this component.
         if (x == parent[x])
             return x;
 
@@ -30,26 +31,36 @@ class Solution {
         parent = new int[n];
         rank = new int[n];
 
-        Arrays.fill(rank, 0);
+        // Each computer is initially in its own set (its own parent).
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
+        // The rank of each individual node is initially 0.
+        Arrays.fill(rank, 0);
 
         int cable = C.length;
 
-        if (cable + 1 < n) //We dont have minimum enough cable
+        // To connect 'n' computers (nodes), you need at least 'n-1' cables (edges).
+        // If we don't have enough cables, it's impossible to connect everyone.
+        if (cable < n - 1)
             return -1;
 
-        int ans = 0;
-
+        // Process all existing connections to form connected components.
+        // The union operation will group computers that are already connected.
         for (var x : C) {
             union(x[0], x[1]);
         }
 
+        // After processing all connections, count the number of separate components.
+        // A component is identified by its root parent (a node that is its own parent).
+        int numberOfComponents = 0;
         for (int i = 0; i < n; i++) {
             if (parent[i] == i)
-                ans++;
+                numberOfComponents++;
         }
-        return ans - 1;
+
+        // To connect 'k' components, you need 'k-1' new cables.
+        // We already confirmed we have enough total cables, answer is the number of components minu - 1.
+        return numberOfComponents - 1;
     }
 }
