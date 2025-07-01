47c2466666de2cc82331a1d05444d482f3c4e680
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-07-01 16:09:37 +0530

    Time: 1 ms (87.19%), Space: 42.1 MB (61.16%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -1,20 +1,18 @@
 class Solution {
     public int possibleStringCount(String word) {
-        Map<Character, Integer> mp = new HashMap<>();
         char[] temp = word.toCharArray();
-
-        for (int i = 0; i < temp.length; i++) {
-            if (!mp.containsKey(temp[i])) {
-                mp.put(temp[i], 1);
-            } else if (temp[i - 1] == temp[i]) {
-                mp.merge(temp[i], 1, Integer::sum);
-            }
-        }
         int ans = 0;
-        for (int x : mp.values()) {
-            if (x > 1)
-                ans += (x - 1);
+
+        char prev = temp[0];
+        for (int i = 1; i < temp.length; i++) {
+            char cur = temp[i];
+
+            if (prev == cur)
+                ans++;
+            else
+                prev = cur;
         }
+
         return ans + 1;
     }
 }
