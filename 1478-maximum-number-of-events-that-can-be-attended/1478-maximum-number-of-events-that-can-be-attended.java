a73ff337739f2f8327da27fdba8a3ef7d2121006
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-07-10 12:42:26 +0530

    Time: 68 ms (23.08%), Space: 76 MB (21.88%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -1,29 +1,48 @@
 class Solution {
-    public int maxEvents(int[][] E) {
-        Arrays.sort(E, (a, b) -> Integer.compare(a[0], b[0]));
+    public int maxEvents(int[][] events) {
+        // Sort events by their start day
+        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // Min-heap to store the end days of available events
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        int i = 0, n = E.length, ans = 0;
-        int days = E[0][0];
+        int eventIndex = 0;
+        int attendedEvents = 0;
+        int n = events.length;
 
-        while (!pq.isEmpty() || i < n) {
-            if (pq.isEmpty())
-                days = E[i][0];
-
-            while (i < n && E[i][0] == days) {
-                pq.offer(E[i][1]);
-                i++;
+        // Iterate through each day, from day 1 up to the max possible day
+        // To be safe, we can find the max end day, but a large enough number works if constraints are known.
+        // A more efficient approach is to loop as long as there are events to process or attend.
+        int currentDay = 0;
+        while (eventIndex < n || !pq.isEmpty()) {
+            // If the queue is empty and we have events left,
+            // jump to the start day of the next event to save time.
+            if (pq.isEmpty()) {
+                currentDay = events[eventIndex][0];
             }
+
+            // Add the end days of all events that start on the currentDay
+            while (eventIndex < n && events[eventIndex][0] == currentDay) {
+                pq.offer(events[eventIndex][1]);
+                eventIndex++;
+            }
+
+            // Remove events from the queue that have already ended
+            while (!pq.isEmpty() && pq.peek() < currentDay) {
+                pq.poll();
+            }
+
+            // If there are events we can attend today, attend one.
+            // We attend the one that ends the soonest (greedy choice).
             if (!pq.isEmpty()) {
                 pq.poll();
-                ans++;
+                attendedEvents++;
             }
-            days++;
 
-            while (!pq.isEmpty() && pq.peek() < days) {
-                pq.poll();
-            }
+            // Move to the next day
+            currentDay++;
         }
-        return ans;
+
+        return attendedEvents;
     }
 }
