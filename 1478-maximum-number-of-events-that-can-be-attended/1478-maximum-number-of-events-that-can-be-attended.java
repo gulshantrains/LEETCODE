044b65a809f8d5545081ed9ba4c30081c07c48b5
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-07-10 12:34:01 +0530

    Time: 66 ms (47.16%), Space: 76 MB (15.11%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int maxEvents(int[][] events) {
-        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        int day = 0, index = 0 , n = events.length ,result = 0;      
-        
+    public int maxEvents(int[][] E) {
+        Arrays.sort(E, (a, b) -> Integer.compare(a[0], b[0]));
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        while (!pq.isEmpty() || index < n) {
-            if (pq.isEmpty()) {
-                day = events[index][0];
-            }
-            while (index < n && events[index][0] <= day) {
-                pq.offer(events[index][1]);
-                index++;
-            }
-            pq.poll();
-            result++; 
-            day++;    
 
-            while (!pq.isEmpty() && pq.peek() < day) {
+        int i = 0, n = E.length, ans = 0;
+        int days = E[0][0];
+
+        while (!pq.isEmpty() || i < n) {
+            while (i < n && E[i][0] == days) {
+                pq.offer(E[i][1]);
+                i++;
+            }
+            if (!pq.isEmpty()) {
+                pq.poll();
+                ans++;
+            }
+            days++;
+            
+            while (!pq.isEmpty() && pq.peek() < days) {
                 pq.poll();
             }
         }
-        return result;
+        return ans;
     }
 }
