3a34359fe4eb16111c9a75b57108165efa94fd5a
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-07-25 16:37:45 +0530

    Time: 6 ms (3.13%), Space: 42.7 MB (39.38%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -1,27 +1,15 @@
 class Solution {
+
     public int maxSum(int[] nums) {
-        Set<Integer> st = new HashSet<>();
-        int ans = 0;
-        int ncnt = 0, minn = Integer.MIN_VALUE;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                ncnt++;
-                minn = Math.max(minn, nums[i]);
+        Set<Integer> positiveNumsSet = new HashSet<>();
+        for (int num : nums) {
+            if (num > 0) {
+                positiveNumsSet.add(num);
             }
         }
-
-        if (ncnt != nums.length) {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] >= 0 && !st.contains(nums[i])) {
-                    ans += nums[i];
-                    st.add(nums[i]);
-                }
-            }
-        } else {
-            return minn;
+        if (positiveNumsSet.isEmpty()) {
+            return Arrays.stream(nums).max().getAsInt();
         }
-
-        return ans;
+        return positiveNumsSet.stream().mapToInt(Integer::intValue).sum();
     }
 }
