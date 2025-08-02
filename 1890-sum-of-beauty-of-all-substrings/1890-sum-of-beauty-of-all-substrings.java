1ecf67253564c0a0ef1d284fc5de3db6214ed8e4
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-08-02 12:33:59 +0530

    Time: 51 ms (96.98%), Space: 43.1 MB (28.73%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -2,15 +2,16 @@ class Solution {
     public int beautySum(String s) {
         int n = s.length();
         int ans = 0;
+        char[] cur = s.toCharArray();
 
         for (int i = 0; i < n; i++) {
             int[] freq = new int[26];
 
             for (int j = i; j < n; j++) {
-                freq[s.charAt(j) - 'a']++;
+                freq[cur[j] - 'a']++;
 
-                int min = 100_000_000;
-                int max = -100_000_000;
+                int min = (int) 1e9;
+                int max = 0;
 
                 for (int f : freq) {
                     if (f > 0) { // Only consider characters present in the substring
