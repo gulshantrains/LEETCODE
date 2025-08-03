b06c02e6da2137e9a2077fdac887abe010b93ab0
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-08-04 01:39:38 +0530

    Time: 2 ms (97.56%), Space: 97.5 MB (60.98%) - LeetHub

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.java
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.java
@@ -5,24 +5,14 @@ class Solution {
         for (int right = 0; right < n; ++right) {
             curSum += A[right][1];
 
-            int l = A[left][0];
-            int r = A[right][0];
-
-            /*
-             (r-l) is Window Size(length of the fruit zone)
-              Math.min(Math.abs(startPos - l), Math.abs(startPos - r)  
-              This calculates the distance to the nearer of the two ends of the fruit zone. 
-              To be efficient, your first move should always be toward the closest entry point of the collection zone.
-            */
-            int cost = (r - l) + Math.min(Math.abs(startPos - l), Math.abs(startPos - r));
-
-            while (left <= right && cost > k) {
+            // (r-l) is Window Size (length of the fruit zone)
+            // Math.min(Math.abs(startPos - l), Math.abs(startPos - r))  
+            // This calculates the distance to the nearer of the two ends of the fruit zone. 
+            // To be efficient, your first move should always be toward the closest entry point of the collection zone.
+            while (left <= right && ((A[right][0] - A[left][0])
+                    + Math.min(Math.abs(startPos - A[left][0]), Math.abs(startPos - A[right][0]))) > k) {
                 curSum -= A[left][1];
                 left++;
-                
-                if (left <= right)
-                    l = A[left][0];
-                cost = (r - l) + Math.min(Math.abs(startPos - l), Math.abs(startPos - r));
             }
             maxSum = Math.max(curSum, maxSum);
         }
