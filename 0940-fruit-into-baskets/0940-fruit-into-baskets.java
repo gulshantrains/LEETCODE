6df95ec8560eaee68d1cc3caa4e4e9d273106f8c
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-08-04 23:25:29 +0530

    Time: 46 ms (84.26%), Space: 55.1 MB (62.92%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -7,11 +7,9 @@ class Solution {
     public int totalFruit(int[] A) {
         Map<Integer, Integer> mp = new HashMap<>();
         int left = 0, ans = 0, n = A.length;
-        int temp = 0;
 
         for (int right = 0; right < n; right++) {
             mp.merge(A[right], 1, Integer::sum);
-            temp++;
 
             while (mp.size() > 2) {
                 mp.merge(A[left], -1, Integer::sum);
@@ -19,14 +17,14 @@ class Solution {
                 if (mp.get(A[left]) <= 0)
                     mp.remove(A[left]);
                 left++;
-                temp--;
             }
-            ans = Math.max(ans, temp);
+            ans = Math.max(ans, right - left + 1);
         }
         return ans;
     }
 }
 /*
+34/91 Test Case Passed
 Map<Integer, Integer> mp = new HashMap<>();
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
