dd5cbe3b915dc14087cca2de03216888acaaeb4a
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-08-07 19:49:56 +0530

    Time: 0 ms (100%), Space: 41.1 MB (49.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,35 +1,31 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-
         if (n == 1)
-            return nums[0];
+            return nums[0]; // Only one house
         if (n == 2)
-            return Math.max(nums[0], nums[1]);
+            return Math.max(nums[0], nums[1]); // Two houses, choose max
+ //dp[i]-->Max profit when we have i House
+        // Case 1: Include first house, exclude last → nums[0] to nums[n-2]
+        int[] dp1 = new int[n + 1];
+        dp1[0] = 0;
+        dp1[1] = nums[0];
+        for (int i = 2; i <= n - 1; i++) {
+            int skip = dp1[i - 1];
+            int take = nums[i - 1] + dp1[i - 2];
+            dp1[i] = Math.max(skip, take);
+            //Will store Result in dp[n-1];
+        }
 
-        // Case 1: Exclude last house, rob from 0 to n-2
-        int[] dp1 = new int[n];
-        Arrays.fill(dp1, -1);
-        int rob1 = robFrom(nums, 0, n - 2, dp1);
+        // Case 2: Exclude first house, include last → nums[1] to nums[n-1]
+        int[] dp2 = new int[n + 1];
+        dp2[1] = 0;
+        dp2[2] = nums[1];
+        for (int i = 3; i <= n; i++) {
+            dp2[i] = Math.max(dp2[i - 1], nums[i - 1] + dp2[i - 2]);
+            //Will store result in dp[n]
+        }
 
-        // Case 2: Exclude first house, rob from 1 to n-1
-        int[] dp2 = new int[n];
-        Arrays.fill(dp2, -1);
-        int rob2 = robFrom(nums, 1, n - 1, dp2);
-
-        return Math.max(rob1, rob2);
-    }
-
-    private int robFrom(int[] nums, int idx, int end, int[] dp) {
-        if (idx > end)
-            return 0;
-
-        if (dp[idx] != -1)
-            return dp[idx];
-
-        int take = nums[idx] + robFrom(nums, idx + 2, end, dp);
-        int skip = robFrom(nums, idx + 1, end, dp);
-
-        return dp[idx] = Math.max(take, skip);
+        return Math.max(dp1[n - 1], dp2[n]);
     }
 }
