9c0ebc5f75d1454bc8161d2d161cbd4278172c63
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-08-07 23:20:09 +0530

    Time: 196 ms (5.04%), Space: 72.7 MB (49.47%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
@@ -2,11 +2,10 @@ class Solution {
     // dp[i][f]: maximum alternating sum starting from index 'i'
     // where f = 1 means next element will be added (even index in subsequence)
     // and f = 0 means next element will be subtracted (odd index in subsequence)
+
     long[][] dp = new long[100001][2];
 
-    // Recursive function to calculate max alternating sum
     public long solve(int[] nums, int start, boolean flag) {
-        // Base case: if start index goes beyond array length, return 0
         if (start >= nums.length)
             return 0;
 
@@ -25,18 +24,15 @@ class Solution {
         // If false, subtract the number (odd position in subsequence)
         long take = (flag ? nums[start] : -nums[start]) + solve(nums, start + 1, !flag);
 
-        // Store and return the maximum of taking or not taking the element
         return dp[start][f] = Math.max(take, noTake);
     }
 
     public long maxAlternatingSum(int[] nums) {
         int n = nums.length;
 
-        // Initialize dp array with -1 (uncomputed states)
         for (var x : dp)
             Arrays.fill(x, -1);
 
-        // Start from index 0, with flag=true (meaning next number will be added)
         return solve(nums, 0, true);
     }
 }
