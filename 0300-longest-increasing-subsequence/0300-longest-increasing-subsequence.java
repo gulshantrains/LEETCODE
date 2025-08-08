7d008909bc64de5467f9b7509a59370903d26012
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-08-09 01:13:22 +0530

    Time: 6 ms (89.1%), Space: 44.4 MB (48.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,37 +1,18 @@
-/*-------------My Mistakes:
-I was Not able to thought of add +1 if i take a number
-I was passing direct value as previous which was very wrong during Memoization
-*/
 class Solution {
-    int[][] dp;
-    int n;
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> res = new ArrayList<>();
 
-    public int lengthOfLIS(int[] A) {
-        n = A.length;
-        dp = new int[n + 1][n + 1]; // dp[idx][prev+1]
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
+        for (int v : nums) {
+            int pos = Collections.binarySearch(res, v);
 
-        return solve(A, 0, -1);
-    }
-
-    public int solve(int[] A, int idx, int prev) {
-        if (idx == n)
-            return 0;
-
-        // Shift prev by +1 so that -1 maps to 0
-        if (dp[idx][prev + 1] != -1)
-            return dp[idx][prev + 1];
-
-        // Case 1: skip current element
-        int skip = solve(A, idx + 1, prev);
-
-        // Case 2: take current element (if allowed)
-        int take = 0;
-        if (prev == -1 || A[prev] < A[idx]) {
-            take = 1 + solve(A, idx + 1, idx);
+            if (pos < 0)
+                pos = -(pos + 1);
+            if (pos == res.size()) {
+                res.add(v);
+            } else {
+                res.set(pos, v);
+            }
         }
-
-        return dp[idx][prev + 1] = Math.max(skip, take);
+        return res.size();
     }
 }
