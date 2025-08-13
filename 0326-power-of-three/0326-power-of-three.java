af0990a786bd34119c30f4021f0064cbedacbd7b
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-08-13 14:03:05 +0530

    Time: 7 ms (100%), Space: 44.6 MB (5.71%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,12 +1,21 @@
+/*
+    3^n < 2^31  
+    log₂(3^n) < log₂(2^31)
+    n * log₂(3) < 31
+    
+    n < 31 / log₂(3)
+    log₂(3) ≈ 1.58496
+    
+    n < 31 / 1.58496 ≈ 19.56 ----> So n value must be less than 20 
+    And if number is prime then its power multiple is just smaller power of that number only
+    
+    */
 class Solution {
+
     public boolean isPowerOfThree(int n) {
-        if(n<=0) return false;
         
-        while (n > 1) {
-            if (n % 3 != 0)
-                return false;
-            n /= 3;
-        }
-        return true;
+        // int x=(int)Math.pow(3,19);
+        // System.out.println(x);
+        return (n > 0 && 1162261467 % n == 0);
     }
 }
