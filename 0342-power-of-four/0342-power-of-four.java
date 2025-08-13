500270d4d4a2fdcb0701566361439131d65a988d
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-08-13 14:27:42 +0530

    Time: 0 ms (100%), Space: 40.9 MB (60.59%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -3,11 +3,17 @@ class Solution {
         if (n <= 0)
             return false;
 
+        return (n > 0) && ((n & n - 1) == 0) && ((0x55555555 & n) != 0);
+    }
+}
+/*
+if (n <= 0)
+            return false;
+
         while (n > 1) {
             if (n % 4 != 0)
                 return false;
             n /= 4;
         }
         return n == 1;
-    }
-}
+*/
