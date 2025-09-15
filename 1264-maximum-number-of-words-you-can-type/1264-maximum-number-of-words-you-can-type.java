f70c828aa794568b9a44c05a848715e1ecf0d4d6
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-09-15 18:35:26 +0530

    Time: 3 ms (48.72%), Space: 41.9 MB (93.74%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -1,4 +1,26 @@
 class Solution {
+    public int canBeTypedWords(String text, String brokeL) {
+        Set<Character> broke = new HashSet<>();
+        for (char c : brokeL.toCharArray()) {
+            broke.add(c);
+        }
+        int count = 1;
+        boolean flag = true;
+
+        for (char x : text.toCharArray()) {
+            if (x == ' ') {
+                count++;
+                flag = true;
+            } else if (broke.contains(x) && flag) {
+                count--;
+                flag = false; //It will prevent to count twice is two words are broke
+            }
+        }
+        return count < 0 ? 0 : count;
+    }
+}
+/*
+class Solution {
     public int canBeTypedWords(String text, String brokLet) {
         Set<Character> broke = new HashSet<>();
         for (char c : brokLet.toCharArray()) {
@@ -23,3 +45,4 @@ class Solution {
         return ans;
     }
 }
+*/
