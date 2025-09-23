5ec36aa3fcb9ade193f6acc0f43b63c4586995fd
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-09-23 06:32:26 +0530

    Time: 1 ms (76.03%), Space: 41.7 MB (48.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -6,12 +6,9 @@ class Solution {
         int i = 0, j = 0;
 
         while (i < v1.length || j < v2.length) {
-            // handle missing parts as 0
-            String s1 = (i < v1.length) ? v1[i] : "0";
-            String s2 = (j < v2.length) ? v2[j] : "0";
-
-            int t1 = Integer.parseInt(removez(s1));
-            int t2 = Integer.parseInt(removez(s2));
+            // if i or j is out of bounds, treat as 0 (Given in Qns otherwise we may get AOI)
+            int t1 = (i < v1.length) ? Integer.parseInt(v1[i]) : 0;
+            int t2 = (j < v2.length) ? Integer.parseInt(v2[j]) : 0;
 
             if (t1 > t2) return 1;
             if (t1 < t2) return -1;
@@ -20,14 +17,5 @@ class Solution {
             j++;
         }
         return 0;
-    }
-
-    public String removez(String x) {
-        int i = 0;
-        // skip leading zeros but stop at last character
-        while (i < x.length() - 1 && x.charAt(i) == '0') {
-            i++;
-        }
-        return x.substring(i);
     }
 }
