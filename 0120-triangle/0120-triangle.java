f4cb841b43d69eefbb6752860320dece126ea32a
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-09-26 02:35:26 +0530

    Time: 10 ms (6%), Space: 46 MB (7%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,16 +1,18 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> tri) {
-        // mem points to the same triangle; we modify in-place
-        List<List<Integer>> mem = tri;
+        
+        List<List<Integer>> mem = new ArrayList<>();
+        for (List<Integer> ls : tri) {
+            mem.add(new ArrayList<>(ls));
+        }
 
         // start from second last row
         for (int row = tri.size() - 2; row >= 0; row--) {
             for (int col = 0; col < tri.get(row).size(); col++) {
                 int newVal = mem.get(row).get(col) + Math.min(
                         mem.get(row + 1).get(col),
-                        mem.get(row + 1).get(col + 1)
-                );
-                mem.get(row).set(col, newVal);  //use set() to update
+                        mem.get(row + 1).get(col + 1));
+                mem.get(row).set(col, newVal); //use set() to update
             }
         }
 
