44a5415791f9c9d1b177a7fe6ce7ab17f49557ae
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-09-30 23:31:00 +0530

    Time: 25 ms (98.66%), Space: 45.6 MB (5.42%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
@@ -1,27 +1,19 @@
 import java.math.BigInteger;
+
 class Solution {
     public int triangularSum(int[] nums) {
         int n = nums.length;
-        if (n == 1)
-            return nums[0];
-
-        BigInteger c = BigInteger.ONE; // start with C(n-1,0) = 1
-        BigInteger[] coeF = new BigInteger[n];
-        coeF[0] = c;
-
-        // compute binomial coefficients exactly (BigInteger)
-        for (int i = 1; i < n; i++) {
-            // c = c * (n-i) / i
-            c = c.multiply(BigInteger.valueOf(n - i))
-                    .divide(BigInteger.valueOf(i));
-            coeF[i] = c;
-        }
-
-        // compute weighted sum modulo 10
+        BigInteger c = BigInteger.ONE; // C(n-1,0)
         BigInteger ans = BigInteger.ZERO;
+
         for (int i = 0; i < n; i++) {
-            ans = ans.add(BigInteger.valueOf(nums[i])
-                    .multiply(coeF[i]));
+            ans = ans.add(c.multiply(BigInteger.valueOf(nums[i])));
+
+            if (i < n - 1) {
+                // compute next coefficient
+                c = c.multiply(BigInteger.valueOf(n - 1 - i))
+                     .divide(BigInteger.valueOf(i + 1));
+            }
         }
 
         return ans.mod(BigInteger.TEN).intValue();
