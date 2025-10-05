74668ab3606745ea482de48a753d1219202c2217
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-10-06 01:29:51 +0530

    Time: 4 ms (97.63%), Space: 46.5 MB (6.34%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,60 +1,37 @@
 class Solution {
-    class Pair {
-        int x, y;
-        Pair(int x, int y) { this.x = x; this.y = y; }
-    }
+    int[][] dir = {{-1,0},{0,1},{1,0},{0,-1}};
+    int m, n;
 
-    public static int[][] dir = { {-1,0}, {0,1}, {1,0}, {0,-1} };
-
-    public void bfs(int[][] hei, boolean[][] ocean) {
-        int m = hei.length, n = hei[0].length;
-        Queue<Pair> q = new LinkedList<>();
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(ocean[i][j])
-                    q.offer(new Pair(i,j));
-            }
-        }
-
-        while(!q.isEmpty()){
-            Pair temp = q.poll();
-            int x = temp.x, y = temp.y;
-
-            for(int[] d : dir){
-                int nx = x + d[0], ny = y + d[1];
-
-                if(nx<0 || ny<0 || nx>=m || ny>=n) continue;
-                if(ocean[nx][ny]) continue; //skip already visited
-                if(hei[nx][ny] < hei[x][y]) continue;
-
-                ocean[nx][ny] = true;
-                q.offer(new Pair(nx, ny));
-            }
+    private void dfs(int[][] heights, boolean[][] ocean, int x, int y) {
+        ocean[x][y] = true;
+        for (int[] d : dir) {
+            int nx = x + d[0], ny = y + d[1];
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+            if (ocean[nx][ny]) continue; // already visited
+            if (heights[nx][ny] < heights[x][y]) continue; // water can only flow from higher/equal
+            dfs(heights, ocean, nx, ny);
         }
     }
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int m = heights.length, n = heights[0].length;
+        m = heights.length;
+        n = heights[0].length;
+
         boolean[][] paci = new boolean[m][n];
         boolean[][] atla = new boolean[m][n];
 
         // Pacific borders
-        for(int i=0;i<m;i++) paci[i][0] = true;
-        for(int j=0;j<n;j++) paci[0][j] = true;
+        for (int i = 0; i < m; i++) dfs(heights, paci, i, 0);
+        for (int j = 0; j < n; j++) dfs(heights, paci, 0, j);
 
         // Atlantic borders
-        for(int i=0;i<m;i++) atla[i][n-1] = true;
-        for(int j=0;j<n;j++) atla[m-1][j] = true;
-
-        bfs(heights, paci);
-        bfs(heights, atla);
+        for (int i = 0; i < m; i++) dfs(heights, atla, i, n - 1);
+        for (int j = 0; j < n; j++) dfs(heights, atla, m - 1, j);
 
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(paci[i][j] && atla[i][j])
-                    ans.add(Arrays.asList(i,j));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (paci[i][j] && atla[i][j]) ans.add(Arrays.asList(i, j));
             }
         }
         return ans;
