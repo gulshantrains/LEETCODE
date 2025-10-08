d7b55853bb57d38a27490c29f1c15b4ab41952cd
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-10-08 07:09:19 +0530

    Time: 32 ms (96.43%), Space: 65 MB (64.94%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,4 +1,39 @@
 class Solution {
+    public int[] successfulPairs(int[] spells, int[] pot, long success) {
+        Arrays.sort(pot);
+        int n = spells.length, m = pot.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int idx = BS(pot, success, spells[i]);
+            
+            ans[i] = (idx == -1) ? 0 : (m - idx);
+        }
+        return ans;
+    }
+
+    // Binary search for first index where pot[mid] * spell >= success
+    private int BS(int[] pot, long success, long spell) {
+        int l = 0, r = pot.length - 1;
+        int ans = -1;
+
+        while (l <= r) {
+            int mid = (l + r) >>> 1;
+            long prod = (long) pot[mid] * spell;
+
+            if (prod >= success) {
+                ans = mid;
+                r = mid - 1;  // look for smaller index
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
+
+/*
+class Solution {
     public int[] successfulPairs(int[] s, int[] pot, long range) {
         int n = s.length, m = pot.length;
         int[] ans = new int[n];
@@ -10,7 +45,7 @@ class Solution {
             if (idx != -1) {
                 long total = m - idx;
 
-                ans[i] = (int)total;
+                ans[i] = (int) total;
             }
         }
         return ans;
@@ -34,3 +69,4 @@ class Solution {
         return ans;
     }
 }
+*/
