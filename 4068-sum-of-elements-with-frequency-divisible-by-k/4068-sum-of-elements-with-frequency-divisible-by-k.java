40ea2343028c1ad6d0aa7f30f00692ad1618b828
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-10-12 16:24:27 +0530

    Time: 3 ms (0%), Space: 42.9 MB (50%) - LeetHub

--- 4068-sum-of-elements-with-frequency-divisible-by-k/4068-sum-of-elements-with-frequency-divisible-by-k.java
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/4068-sum-of-elements-with-frequency-divisible-by-k.java
@@ -1,15 +1,30 @@
 class Solution {
     public int sumDivisibleByK(int[] nums, int k) {
-      int[] freq=new int[101];
-      int ans=0;
+        int ans=0;
+        Map<Integer,Integer> mp=new HashMap<>();
+        for(var x: nums) mp.merge(x,1,Integer::sum);
 
-      for(int x: nums) freq[x]++;
-
-      for(int i=0; i<101; i++){
-        if(freq[i]%k==0)
-            ans+=(freq[i]*i);
-    
-      }
-      return ans;
+        for(var x: mp.entrySet()){
+            if(x.getValue()%k==0) ans+=(x.getValue()*x.getKey());
+        }
+        return ans;
     }
 }
+/*
+class Solution {
+    public int sumDivisibleByK(int[] nums, int k) {
+        int[] freq = new int[101];
+        int ans = 0;
+
+        for (var x : nums)
+            freq[x]++;
+
+        for (int i = 0; i < 101; i++) {
+            if (freq[i] % k == 0)
+                ans += (freq[i] * i);
+
+        }
+        return ans;
+    }
+}
+*/
