439c498b793a0fcb8e722170d3425bcdd5a7c779
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-10-15 12:30:50 +0530

    Time: 49 ms (11.7%), Space: 83.1 MB (72.34%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
@@ -1,34 +1,5 @@
 class Solution {
     public int maxIncreasingSubarrays(List<Integer> nums) {
-        int n = nums.size();
-        int[] res = new int[n];
-        res[n - 1] = 1;
-        int ans = 0;
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums.get(i) < nums.get(i + 1)) {
-                res[i] = res[i + 1] + 1;
-            } else {
-                res[i] = 1;
-            }
-        }
-        int i=0;
-        while (i < n) {
-            int cur = res[i];
-
-            ans = Math.max(ans, cur / 2);
-
-            if (cur + i < n) {
-                ans = Math.max(ans, Math.min(cur, res[cur + i]));
-            }
-            i = cur + i;
-        }
-        return ans;
-    }
-}
-/*
-class Solution {
-    public int maxIncreasingSubarrays(List<Integer> nums) {
         Stack<Integer> st = new Stack<>();
         int n = nums.size(), prev = -1, ans = 0, i = 0;
 
@@ -43,6 +14,8 @@ class Solution {
             }
             int currLen = st.size();
 
+            ans=Math.max(ans, currLen/2);
+
             if (prev != -1) { //We have atleast 2 subArray
                 ans = Math.max(ans, Math.min(currLen, prev));
             }
@@ -51,4 +24,3 @@ class Solution {
         return ans;
     }
 }
-*/
