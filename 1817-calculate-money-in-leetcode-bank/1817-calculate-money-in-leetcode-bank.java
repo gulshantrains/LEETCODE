dd1e01501a22a131a59c9009261e222c81dc95e0
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-10-25 21:40:20 +0530

    Time: 0 ms (100%), Space: 40.8 MB (22.22%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -1,8 +1,8 @@
 class Solution {
     public int totalMoney(int n) {
-        int di = n / 7;
-        int re = n % 7;
-
-        return ((7 * di * di + 49 * di) + (re * (2 * di + re + 1))) >> 1;
+        int full_week = n / 7;
+        int rem_days = n % 7;
+        
+        return ((7 * full_week * full_week + 49 * full_week) + (rem_days * (2 * full_week + rem_days + 1))) >> 1;
     }
 }
