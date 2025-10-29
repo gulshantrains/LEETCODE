055a232e34048fe697c6b8a6d0584e836812ace2
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-10-29 22:52:01 +0530

    Time: 3 ms (66.55%), Space: 54.4 MB (12.65%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,19 +1,16 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        boolean[] per = new boolean[n + 1];
-        int[] people = new int[n + 1];
+        int[] score = new int[n + 1];
 
-        for (int[] x : trust) {
-            per[x[0]] = true;
-            people[x[1]]++;
-
+        for (int[] t : trust) {
+            score[t[0]]--; // trusts someone => lose 1 point
+            score[t[1]]++; // trusted by someone => gain 1 point
         }
 
         for (int i = 1; i <= n; i++) {
-            if (!per[i] && people[i] == n - 1)
+            if (score[i] == n - 1)
                 return i;
         }
-
         return -1;
     }
 }
