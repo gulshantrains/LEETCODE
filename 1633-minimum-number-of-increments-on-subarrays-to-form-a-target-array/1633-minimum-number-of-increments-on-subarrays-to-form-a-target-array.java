062143bd32c504fc082da6c40b7625aca155e422
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-10-31 13:15:29 +0530

    Time: 4 ms (45.27%), Space: 74.9 MB (5.44%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -1,11 +1,12 @@
 class Solution {
-
     public int minNumberOperations(int[] target) {
-        int n = target.length;
-        int ans = target[0];
-        for (int i = 1; i < n; ++i) {
-            ans += Math.max(target[i] - target[i - 1], 0);
+        int n = target.length, op = target[0];
+
+        for (int i = 1; i < n; i++) {
+            if (target[i] > target[i - 1]) {
+                op += (target[i] - target[i - 1]);
+            }
         }
-        return ans;
+        return op;
     }
 }
