432bf99686d2b44c1cb8c499662cf66df74dc78f
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2025-11-01 15:55:55 +0530

    Time: 5 ms (97.39%), Space: 147.1 MB (5.52%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
@@ -1,36 +1,20 @@
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        Arrays.sort(nums); 
+        boolean[] exists = new boolean[100001]; // 1-indexed
+        for (int x : nums)
+            exists[x] = true;
 
         ListNode dummy = new ListNode();
         ListNode prev = dummy, curr = head;
 
         while (curr != null) {
-            if (!exists(nums, curr.val)) {
+            if (!exists[curr.val]) { // only keep if not in nums
                 prev.next = curr;
                 prev = curr;
             }
             curr = curr.next;
         }
-
-        // terminate the list properly
-        prev.next = null;
-
+        prev.next = null; // terminate
         return dummy.next;
-    }
-
-    private boolean exists(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) >> 1;
-
-            if (nums[mid] == target)
-                return true;
-            else if (nums[mid] < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-        return false;
     }
 }
