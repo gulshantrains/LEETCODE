de0e5357883e9cafb8d13de287f70717c30c7190
Gulshan Kumar <137637995+gulshantrains@users.noreply.github.com>
2026-02-21 10:28:02 +0530

    Time: 4 ms (87.21%), Space: 42.5 MB (44.84%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPrime(int n){
-        if(n<=1) return false;
-        if(n<=3) return true;
+        if(n<2) return false;
 
         for(int i=2; i*i<=n; i++){
             if(n%i==0) return false;
